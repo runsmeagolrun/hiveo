c93e766cc48eed0757b60731c8543f1953fcf6c5
Sumit Jadhav <sumit.jadhav@doktor.se>
2024-01-03 10:09:29 +0100

    feat: add landing page

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your workspace to write, organize, and collaborate. Welcome to {""}
+        <span className="underline">Hiveo</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Join a global movement. Unleash your creativity.
+      </h3>
+      <Button>
+        Enter Hiveo
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
