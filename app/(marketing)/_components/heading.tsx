567715205764b8dc39ee4f888ab5ae91a8b7a1f9
Sumit Jadhav <sumit.jadhav@doktor.se>
2024-01-04 17:27:27 +0100

    feat: routing to buttons

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
+  const user = useCurrentUser();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,10 +17,22 @@ export const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Join a global movement. Unleash your creativity.
       </h3>
-      <Button>
-        Enter Hiveo
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {!!user && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Hiveo
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!user && (
+        <Button asChild>
+          <Link href="/register">
+            Get Hiveo free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
     </div>
   );
 };
