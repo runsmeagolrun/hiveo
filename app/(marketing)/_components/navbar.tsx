29c0da6320ba113f10b5bdd3efaeefdd855f7e22
Sumit Jadhav <sumit.jadhav@doktor.se>
2024-01-04 17:28:04 +0100

    feat: update navbar login buttons for routing

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,9 +4,16 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+import { LogoutButton } from "@/components/auth/logout-button";
+import { RegisterButton } from "@/components/auth/register-button";
+import { useCurrentUser } from "@/hooks/use-current-user";
 
 export const Navbar = () => {
   const scrolled = useScrollTop();
+  const user = useCurrentUser();
+
   return (
     <div
       className={cn(
@@ -16,7 +23,23 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        <ModeToggle />
+        {!user && (
+          <LoginButton>
+            <Button variant="ghost" size="sm">
+              Log in
+            </Button>
+          </LoginButton>
+        )}
+        {!user && (
+          <RegisterButton>
+            <Button size="sm">Get Hiveo free</Button>
+          </RegisterButton>
+        )}
+        {!!user && <LogoutButton>Sign out</LogoutButton>}
+
+        <div className="mr-4 pr-4">
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
