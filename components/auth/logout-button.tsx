64798c04639d7c06da07cfdbe00dbb50b06f6d10
Sumit Jadhav <sumit.jadhav@doktor.se>
2024-01-04 17:09:21 +0100

    feat: add logout button

---
+++ components/auth/logout-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { logout } from "@/actions/logout";
+import { Button } from "../ui/button";
+
+interface LogoutButtonProps {
+  children?: React.ReactNode;
+}
+
+export const LogoutButton = ({ children }: LogoutButtonProps) => {
+  const onClick = () => {
+    logout();
+  };
+
+  return (
+    <Button onClick={onClick} className="cursor-pointer">
+      {children}
+    </Button>
+  );
+};
