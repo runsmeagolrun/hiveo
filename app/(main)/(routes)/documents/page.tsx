126cef97bb940e7a1ee3583f53694a3fa5401397
Sumit Jadhav <sumit.jadhav@doktor.se>
2024-01-10 12:21:12 +0100

    feat: add new document feature

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,46 @@
 
 import Image from "next/image";
 import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
 
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { Button } from "@/components/ui/button";
+import { newDocument } from "@/actions/new-document";
 
 const DocumentsPage = () => {
   const user = useCurrentUser();
+
+  const dummyData = {
+    title: "Untitled",
+    userId: "clqz0168300005fy89mpz271l",
+    isArchived: false,
+    content: "content",
+    coverImage: "images/cover",
+    icon: "icon-folder",
+    isPublished: false,
+    parentDocument: [
+      {
+        title: "new document",
+        userId: "clqz0168300005fy89mpz271l",
+        isArchived: false,
+        content: "content",
+        coverImage: "images/cover",
+        icon: "icon-folder",
+        isPublished: false,
+        parentDocument: [],
+      },
+    ],
+  };
+
+  const addDocument = () => {
+    const docPromise = newDocument(dummyData);
+
+    toast.promise(docPromise, {
+      loading: "Creating new document...",
+      success: "New document created!",
+      error: "Failed to create a new document.",
+    });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -28,7 +62,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {!!user?.name ? `${user?.name}\`s` : ""} Hiveo
       </h2>
-      <Button>
+      <Button onClick={addDocument}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
